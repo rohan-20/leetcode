1faf00ba25dee6a4fcb24ccf2e13014fda7a382a
Rohan Sharma <20rohan.sharma08@gmail.com>
2025-10-09 15:53:36 +0530

    updated code

--- contains_duplicate.js
+++ contains_duplicate.js
@@ -1,6 +1,6 @@
-let nums = [1,2,3,4];
+let nums = [1,2,3,3];
 
-function containsDuplicate(){ 
+function containsDuplicate(nums){ 
   let set = new Set();
   for(let i=0; i<nums.length; i++){
     if(!set.has(nums[i])){
@@ -12,4 +12,4 @@ function containsDuplicate(){
   return false;
 }
 
-console.log(containsDuplicate());
+console.log(containsDuplicate(nums));
